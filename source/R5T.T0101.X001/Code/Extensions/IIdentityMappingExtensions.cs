ef73c105f8e63e6f24b3109a730200b304d3e039
David Coats <david@getsafetycone.com>
2022-03-06 16:46:37 -0800

    Add FindDictionaryByLocalIdentity().

--- source/R5T.T0101.X001/Code/Extensions/IIdentityMappingExtensions.cs
+++ source/R5T.T0101.X001/Code/Extensions/IIdentityMappingExtensions.cs
@@ -33,6 +33,30 @@ namespace System.Linq
             return output;
         }
 
+        public static Dictionary<Guid, WasFound<T>> FindDictionaryByLocalIdentity<T>(this IEnumerable<T> items,
+            IEnumerable<Guid> localIdentities)
+            where T : IIdentityMapping
+        {
+            var identitiesHash = new HashSet<Guid>(localIdentities);
+
+            var foundItems = items
+                .Where(x => identitiesHash.Contains(x.LocalIdentity))
+                .Now();
+
+            var notFoundIdentities = identitiesHash.Except(foundItems
+                .Select(x => x.LocalIdentity));
+
+            var output = foundItems
+                .Select(x => (x.LocalIdentity, WasFound.Found(x)))
+                .Concat(notFoundIdentities
+                    .Select(x => (x, WasFound.NotFound<T>())))
+                .ToDictionary(
+                    x => x.Item1,
+                    x => x.Item2);
+
+            return output;
+        }
+
         public static WasFound<T> FindSingleByExternalIdentity<T>(this IEnumerable<T> items, Guid identity)
             where T : IIdentityMapping
         {
